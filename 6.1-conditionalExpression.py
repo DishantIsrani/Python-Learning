3446331c213d98faa06689a2b738fd4816bc8d9a
Dishant Israni <isranidishant14@gmail.com>
2021-10-24 01:31:46 +0530

    Day 3 - Learning Python

---
+++ 6.1-conditionalExpression.py
@@ -0,0 +1,29 @@
+a = 15
+
+# if(a < 3):
+#     print("The value of a is greater than 3")
+# elif(a>7):
+#     print("The value of a is greater than 7")
+# elif(a>27):
+#     print("The value of a is greater than 27")
+# elif(a>37):
+#     print("The value of a is greater than 37")
+# else:
+#     print("The value of a is not greater than 3 or 7")
+    
+
+
+# MULTIPLE IF 
+if(a < 3):
+    print("The value of a is greater than 3")
+
+if(a>7):
+    print("The value of a is greater than 7")
+
+if(a>27):
+    print("The value of a is greater than 27")
+
+if(a>37):
+    print("The value of a is greater than 37")
+else:
+    print("The value of a is not greater than 3 or 7")
---
+++ 6.2-in_and_is.py
@@ -0,0 +1,14 @@
+# USE OF IS or you can us == in case of is 
+ 
+a = None
+if( a is None):
+    print("yes")
+else:
+    print("no")
+
+
+# USE OF IN 
+b=[34, 56, 78]
+print(34 in b)
+print(409273 in b)
+
